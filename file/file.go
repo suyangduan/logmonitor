fd84617c26874e26668a045e1c0d4fadc789c195
Suyang <suyang@Suyangs-MacBook-Air.local>
2023-08-19 15:58:07 -0700

    feat: implement ReadLastLines that reads the last 128 bytes of a file and return the whole lines in reverse order

--- file/file.go
+++ file/file.go
@@ -4,17 +4,16 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"math/rand"
 	"os"
 	"time"
 )
 
 func WriteFileHelper(fileName string, numOfLines int) {
-	// open output file
 	fo, err := os.Create(fileName)
 	if err != nil {
 		panic(err)
 	}
-	// close fo on exit and check for its returned error
 	defer func() {
 		if err := fo.Close(); err != nil {
 			panic(err)
@@ -29,14 +28,25 @@ func WriteFileHelper(fileName string, numOfLines int) {
 	}
 }
 
-func WriteFileHelper2(fileName string, numOfLines int) {
-	fileByte := []byte{}
-	for ; numOfLines > 0; numOfLines-- {
-		newLine := fmt.Sprintf("%s\n", time.Now())
-		fileByte = append(fileByte, []byte(newLine)...)
+func GenerateTestFile(fileName string, numOfLines int) {
+	fo, err := os.Create(fileName)
+	if err != nil {
+		panic(err)
 	}
+	defer func() {
+		if err := fo.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	animals := []string{"mouse", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "ram", "monkey", "rooster", "dog", "pig"}
+	for i := 0; i < numOfLines; i++ {
 
-	os.WriteFile(fileName, fileByte, os.ModePerm)
+		newLine := fmt.Sprintf("%s%d%s%s\n",
+			"Line ", i+1, " this line contains a random animal: ", animals[rand.Intn(12)])
+		if _, err := fo.Write([]byte(newLine)); err != nil {
+			panic(err)
+		}
+	}
 }
 
 func LineCounter(r io.Reader) (int, error) {
@@ -56,4 +66,63 @@ func LineCounter(r io.Reader) (int, error) {
 			return count, err
 		}
 	}
+}
+
+const READ_BUFFER_SIZE = 128
+
+// TODO: no lb within the first scan
+func ReadLastLines(fileName string) ([]string, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	buf := make([]byte, READ_BUFFER_SIZE)
+	stat, err := os.Stat(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	fileSize := stat.Size()
+	curBufStart := fileSize - READ_BUFFER_SIZE
+	if fileSize < READ_BUFFER_SIZE {
+		curBufStart = 0
+	}
+	_, err = file.ReadAt(buf, curBufStart)
+	if err != nil {
+		panic(err)
+	}
+
+	// find all the line break's locations (their index within the buffer)
+	offset := 0
+	indices := []int64{}
+	for {
+		index := bytes.IndexByte(buf[offset:], '\n')
+		// no more line breaks
+		if index == -1 {
+			break
+		}
+
+		indices = append(indices, int64(index+offset))
+		offset += index + 1
+	}
+
+	lastLines := []string{}
+	for i := 0; i < len(indices)-1; i++ {
+		// create a buffer the size between two line breaks (excluding the second line break)
+		// and then read everything in between
+		endLineBreakIndex := indices[len(indices)-1-i]
+		startLineBreakIndex := indices[len(indices)-2-i]
+		bufferSize := endLineBreakIndex - startLineBreakIndex - 1
+		newBuf := make([]byte, bufferSize)
+		_, err = file.ReadAt(newBuf, curBufStart+startLineBreakIndex+1)
+		if err == nil {
+			lastLines = append(lastLines, string(newBuf))
+		} else {
+			return lastLines, err
+		}
+	}
+
+	return lastLines, nil
 }
--- file/file_test.go
+++ file/file_test.go
@@ -33,28 +33,34 @@ var _ = Describe("write file helper", func() {
 	})
 })
 
-var _ = Describe("write file helper 2", func() {
-	It("work as expected", func() {
-		fileName := "test.txt"
-		Expect(true).To(BeTrue())
-		file.WriteFileHelper2(fileName, 100)
+//var _ = Describe("This test generates a test file", func() {
+//	It("", func() {
+//		file.GenerateTestFile("1KB.txt", 100)
+//	})
+//})
 
-		// open input file
-		fi, err := os.Open(fileName)
-		if err != nil {
-			panic(err)
+var _ = Describe("Read last line", func() {
+	It("works as expected for 100 line file (5KB)", func() {
+		expectedLines := []string{
+			"Line 100 this line contains a random animal: dragon",
+			"Line 99 this line contains a random animal: mouse",
 		}
-		// close fi on exit and check for its returned error
-		defer func() {
-			if err := fi.Close(); err != nil {
-				panic(err)
-			}
-			os.Remove(fileName)
-		}()
+		Expect(file.ReadLastLines("5KB.txt")).To(Equal(expectedLines))
+	})
 
-		numOfLines, err := file.LineCounter(fi)
+	It("works as expected for 100k line file (5MB)", func() {
+		expectedLines := []string{
+			"Line 100000 this line contains a random animal: pig",
+			"Line 99999 this line contains a random animal: mouse",
+		}
+		Expect(file.ReadLastLines("5MB.txt")).To(Equal(expectedLines))
+	})
 
-		Expect(err).To(BeNil())
-		Expect(numOfLines).To(Equal(100))
+	It("works as expected for 20M line file (1GB)", func() {
+		expectedLines := []string{
+			"Line 20000000 this line contains a random animal: mouse",
+			"Line 19999999 this line contains a random animal: horse",
+		}
+		Expect(file.ReadLastLines("1GB.txt")).To(Equal(expectedLines))
 	})
 })
