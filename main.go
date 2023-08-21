9f79bfa2a8d16ba0e033e9c961c03103e1c6ea1b
Suyang <suyang@Suyangs-MacBook-Air.local>
2023-08-20 17:26:46 -0700

    feat: implement simple REST API server

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"cribl/logmonitor/file"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+const FILE_PATH = "/var/log/"
 
 func main() {
-	fmt.Println("hello world")
+	router := gin.Default()
+
+	router.GET("/api/v1/logs", func(c *gin.Context) {
+		size := c.DefaultQuery("size", "100")
+		filename := c.DefaultQuery("filename", "var5MB.txt")
+		searchKeyword := c.Query("keyword")
+
+		numOfEntries, err := strconv.Atoi(size)
+		if err != nil {
+			c.Error(err)
+		}
+
+		filenameWithPath := FILE_PATH + filename
+
+		result, err := file.ReadLastNLinesWithKeyword(filenameWithPath, numOfEntries, searchKeyword)
+		if err != nil {
+			c.Error(err)
+		}
+
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.IndentedJSON(http.StatusOK, result)
+	})
+
+	router.Run("localhost:8080")
 }
